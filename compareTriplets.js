deae7e5565469a7970c0b52c58891a8fe000e401
principlebrothers <principlebrothers.com>
2022-10-20 08:10:39 +0000

    Solve Compare Triplets

---
+++ compareTriplets.js
@@ -0,0 +1,16 @@
+function compareTriplets(a, b) {
+  let alice = 0;
+  let bob = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) {
+      alice++;
+    } else if (a[i] < b[i]) {
+      bob++;
+    }
+  }
+  return [alice, bob];
+}
+
+let a = [5, 6, 7];
+let b = [3, 6, 10];
+console.log(compareTriplets(a, b));
