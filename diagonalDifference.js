b4755a4516c0bcd6679fa50a8d25bad1840396dc
principlebrothers <principlebrothers.com>
2022-10-20 08:21:20 +0000

    Solve diagonal difference challenge

---
+++ diagonalDifference.js
@@ -0,0 +1,17 @@
+const diagonalDifference = (arr) => {
+  let leftToRight = 0;
+  let rightToLeft = 0;
+  for (let i = 0; i < arr.length; i++) {
+    leftToRight += arr[i][i];
+    rightToLeft += arr[i][arr.length - 1 - i];
+  }
+  return Math.abs(leftToRight - rightToLeft);
+}
+
+const arr = [
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12],
+];
+
+console.log(diagonalDifference(arr))
