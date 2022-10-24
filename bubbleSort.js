8ba889ba38155d8cbfd24ea0290875554f547ff0
Ernest Anyewe Adonu <ernestadonu@yahoo.com>
2022-10-24 23:59:38 +0000

    Create bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+const bubbleSort = (arr) => {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let tmp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+};
+
+let arr = [5, 3, 8, 2, 1, 4];
+console.log(bubbleSort(arr));
